13b84c6c8f674a18c95d786dcc8cde4f3d5d04b5
Tay CJ <cjtay@outlook.sg>
2020-08-14 17:58:49 +0800

    imac include alt arrow functin in bread actions

--- src/redux/bread/breadActions.js
+++ src/redux/bread/breadActions.js
@@ -1,8 +1,13 @@
 import { BUY_BREAD } from './breadTypes';
 
-export const buyBread = (number) => {
-    return {
-        type: BUY_BREAD,
-        payload: number,
-    };
-};
+export const buyBread = (number) => ({
+    type: BUY_BREAD,
+    payload: number,
+});
+
+// export const buyBread = (number) => {
+//     return {
+//         type: BUY_BREAD,
+//         payload: number,
+//     };
+// };
